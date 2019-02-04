18119a5f6cf44edc74e9f8c5709cd288b3d6ec37
Neta Globerman <neta.globerman@gmail.com>
2019-02-04 12:56:04 +0200

    exercise5 - ssh - server1 to server2 - passwordless & no host checking

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+
+su vagrant -c "ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -q -N \"\""
+cp /home/vagrant/.ssh/id_rsa.pub /vagrant/
+
+echo "Host server2
+    User vagrant
+    HostName server2
+    IdentityFile /home/vagrant/.ssh/id_rsa
+    StrictHostKeyChecking no" > /home/vagrant/.ssh/config
+
--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
+
+cat /vagrant/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
